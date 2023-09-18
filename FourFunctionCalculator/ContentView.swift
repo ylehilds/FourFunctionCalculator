0a8fe1073133f0e07ce577f05b696bf885258e67
ylehilds <lehi_alcantara@byu.edu>
2023-09-18 13:39:33 -0600

    Initial Commit

---
+++ FourFunctionCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FourFunctionCalculator
+//
+//  Created by Lehi Alcantara on 9/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
